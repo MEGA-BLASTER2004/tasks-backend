a49c7da86a767ff567a5a2a77eece1f09ee1169e
--add <ojastyagi@outlook.com>
2021-11-11 21:10:02 +0530

    CHANGES

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.json());
-app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("HOME");
